a9d2c9f899b72d83d219eb640e104aa959faf0b7
jamietti <jaanam@kolumbus.fi>
2017-05-20 00:27:04 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, json
+from model import db, app, Companies, Stocks, Stockvalues 
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
+from flask_admin.contrib.sqla import ModelView
+from datetime import datetime
+from dateutil.parser import parse
+import pandas as pd
+import operator
+
+app.config['SQLALCHEMY_ECHO'] = True
+Session(app)
+
+def Max(a, b): return a if a > b else b
+
+@app.route("/",methods=['POST', 'GET'])
+def main():
+  data = list()  
+  for stock in db.session.query(Stocks):
+    data.append(stock.as_tuple())
+  return render_template('stocks.html',data=data)
+   
+@app.route('/showShares/',methods=['POST'])
+def showShares():
+  alku=request.form['alkupvm']
+  loppu=request.form['loppupvm']
+  data = list()
+  kasvut={}
+  for stock_id in db.session.query(Stocks):
+    eka = db.session.query(Stockvalues).filter(Stockvalues.stockId==stock_id.id,Stockvalues.valueDate >= pd.to_datetime(alku)).order_by(Stockvalues.valueDate).first()
+    alkuarvo=eka.close
+    pienin=eka.valueDate
+    for apu in db.session.query(Stockvalues).filter(Stockvalues.stockId==stock_id.id,Stockvalues.valueDate >= pd.to_datetime(alku)).order_by(Stockvalues.valueDate):
+      pass
+    suurin=apu.valueDate
+    loppuarvo=apu.close
+    kasvut[ stock_id.id ] = ( loppuarvo - alkuarvo )/alkuarvo * 100    
+  sorted_kasvut=sorted(kasvut.items(),key=lambda x: (-x[1], x[0]))
+
+  for kasvu in sorted_kasvut:
+    stock=db.session.query(Stocks).filter(Stocks.id==list(kasvu)[0]).first()
+    company=db.session.query(Companies).filter(Companies.companyId==stock.id).first()   
+    data.append((company.companyName, company.industryField, list(kasvu)[1]))
+  return render_template('kasvut.html',alku=alku, loppu=loppu, data=data)
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0')
---
+++ model.py
@@ -0,0 +1,75 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////root/stocks8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stocks.db'
+db = SQLAlchemy(app)
+
+class Companies(db.Model):
+    companyId = db.Column(db.Integer, primary_key=True)
+    companyName = db.Column(db.String(80), nullable=False)
+    industryField = db.Column(db.String(80), nullable=False)
+    country = db.Column(db.String(80), nullable=False)
+    def __init__(self,companyId, companyName, industryField, country):
+        self.companyId = companyId
+        self.companyName = companyName
+        self.industryField = industryField
+        self.country = country
+    def __repr__(self):
+        return '<Companies %r>' % self.companyName
+    def as_dict(self):
+	return {'companyId' : self.companyId, 'companyName' : self.companyName, 'industryField' : self.industryField, 'country' : self.country}
+    def as_tuple(self):    
+        company, stock = db.session.query(Companies, Stocks
+                                                 ).filter(Stocks.companyId == self.companyId).first()
+                      
+        print "model: comp ", company
+        print "model: st ", stock
+        return (company.companyName, stock.ticker, company.industryField)
+
+class Stocks(db.Model):
+    ticker = db.Column(db.String(80))
+    id = db.Column(db.Integer, primary_key=True)
+    companyId = db.Column(db.Integer, db.ForeignKey(Companies.companyId))
+    def __init__(self,ticker, id, companyId):
+        self.ticker = ticker
+        self.id = id
+        self.companyId = companyId
+    def __repr__(self):
+        return '<Stocks %r>' % self.ticker
+    def as_dict(self):
+        company, stock = db.session.query(Companies, Stocks
+                                                 ).filter(self.companyId == Companies.companyId).first()
+
+        return {'companyName' : company.companyName, 'ticker' : self.ticker, 'industryField' : company.industryField}
+    def as_tuple(self):
+        company, stock = db.session.query(Companies, Stocks
+                                                 ).filter(self.companyId == Companies.companyId).first()
+        return (company.companyName, self.ticker, company.industryField)
+
+class Stockvalues(db.Model):
+    valueDate = db.Column(db.DateTime)
+    stockId = db.Column(db.Integer, db.ForeignKey(Stocks.id))
+    open = db.Column(db.Float, nullable=False)
+    close = db.Column(db.Float)
+    volume = db.Column(db.Integer, nullable=False)
+    valueId = db.Column(db.Integer, primary_key=True)
+    def __init__(self, valueDate, stockId, open, close, volume, valueId):
+        self.valueDate = valueDate
+        self.stockId = stockId
+        self.open = open
+        self.close = close
+        self.volume = volume
+        self.valueId = valueId   
+    def as_dict(self):
+        value = db.session.query(Stockvalues).first()
+        return {'valueDate' : value.valueDate, 'stockId' : value.stockId, 'open' : value.open, 'close' : value.close,'volume' : value.volume, 'valueId' : value.valueId}
+    def as_tuple(self):
+        value = db.session.query(Stockvalues).first()
+        return (value.valueDate, value.stockId, value.open, value.close, value.volume, value.valueId)
+
+
+    def __repr__(self):
+        return '<Stockvalues %r>' % self.stockId
