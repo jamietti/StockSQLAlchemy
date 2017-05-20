a28f0ccefd7b7b165b96b0db65555420a615ab68
root <root@euca-10-254-45-239.eucalyptus.internal>
2017-05-20 08:09:00 +0300

    added time stamp validation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, json, flash
 from model import db, app, Companies, Stocks, Stockvalues 
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
@@ -9,10 +9,13 @@ from dateutil.parser import parse
 import pandas as pd
 import operator
 
-app.config['SQLALCHEMY_ECHO'] = True
+#app.config['SQLALCHEMY_ECHO'] = True
+
+app.secret_key = 'some_secret'
 Session(app)
 
-def Max(a, b): return a if a > b else b
+class ValidationError(Exception):
+    pass
 
 @app.route("/",methods=['POST', 'GET'])
 def main():
@@ -25,19 +28,22 @@ def main():
 def showShares():
   alku=request.form['alkupvm']
   loppu=request.form['loppupvm']
-  data = list()
+ 
   kasvut={}
-  for stock_id in db.session.query(Stocks):
-    eka = db.session.query(Stockvalues).filter(Stockvalues.stockId==stock_id.id,Stockvalues.valueDate >= pd.to_datetime(alku)).order_by(Stockvalues.valueDate).first()
-    alkuarvo=eka.close
-    pienin=eka.valueDate
-    for apu in db.session.query(Stockvalues).filter(Stockvalues.stockId==stock_id.id,Stockvalues.valueDate >= pd.to_datetime(alku)).order_by(Stockvalues.valueDate):
-      pass
-    suurin=apu.valueDate
-    loppuarvo=apu.close
-    kasvut[ stock_id.id ] = ( loppuarvo - alkuarvo )/alkuarvo * 100    
-  sorted_kasvut=sorted(kasvut.items(),key=lambda x: (-x[1], x[0]))
-
+  try:
+    for stock_id in db.session.query(Stocks):
+      eka = db.session.query(Stockvalues).filter(Stockvalues.stockId==stock_id.id,Stockvalues.valueDate >= pd.to_datetime(alku)).order_by(Stockvalues.valueDate).first()
+      alkuarvo=eka.close
+      pienin=eka.valueDate
+      for apu in db.session.query(Stockvalues).filter(Stockvalues.stockId==stock_id.id,Stockvalues.valueDate <= pd.to_datetime(loppu)).order_by(Stockvalues.valueDate):
+        pass
+      suurin=apu.valueDate
+      loppuarvo=apu.close
+      kasvut[ stock_id.id ] = ( loppuarvo - alkuarvo )/alkuarvo * 100    
+    sorted_kasvut=sorted(kasvut.items(),key=lambda x: (-x[1], x[0]))
+  except Exception as e:
+    return (e.args[0], "error")
+  data = list()     
   for kasvu in sorted_kasvut:
     stock=db.session.query(Stocks).filter(Stocks.id==list(kasvu)[0]).first()
     company=db.session.query(Companies).filter(Companies.companyId==stock.id).first()   
