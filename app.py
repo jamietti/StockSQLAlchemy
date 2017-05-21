dd576b085d7d7f64b4b7de26f702bb889c939350
root <root@euca-10-254-45-239.eucalyptus.internal>
2017-05-21 05:31:07 +0300

    Added check if close is NULL

--- app.py
+++ app.py
@@ -28,39 +28,36 @@ def main():
 def showShares():
   alku=request.form['alkupvm']
   loppu=request.form['loppupvm']
- 
   kasvut={}
-
   for stock_id in db.session.query(Stocks):
     try:
       eka = db.session.query(Stockvalues).filter(Stockvalues.stockId==stock_id.id,Stockvalues.valueDate >= pd.to_datetime(alku)).order_by(Stockvalues.valueDate).first()
     except Exception as e:
       return (e.args[0], "error")
     alkuarvo=eka.close
-    pienin=eka.valueDate
     try: 
       ret= db.session.query(Stockvalues).filter(Stockvalues.stockId==stock_id.id,Stockvalues.valueDate <= pd.to_datetime(loppu)).order_by(Stockvalues.valueDate).all()
     except Exception as e:
       return (e.args[0], "error")
-    if ret:
+
+    if ret and alkuarvo:
       for apu in ret:
-        pass
-      if apu:  
-      	suurin=apu.valueDate
-      	loppuarvo=apu.close
-      	kasvut[ stock_id.id ] = ( loppuarvo - alkuarvo )/alkuarvo * 100
-      else:
-        return("No stock values found for given period")
+        pass  
+      loppuarvo=apu.close
+      kasvut[ stock_id.id ] = ( loppuarvo - alkuarvo )/alkuarvo * 100 
     else:
-      return("No stock values found for given period")    
+      kasvut[ stock_id.id ] = 0
   sorted_kasvut=sorted(kasvut.items(),key=lambda x: (-x[1], x[0]))
-#  except Exception as e:
-#    return (e.args[0], "error")
+
   data = list()     
   for kasvu in sorted_kasvut:
     stock=db.session.query(Stocks).filter(Stocks.id==list(kasvu)[0]).first()
     company=db.session.query(Companies).filter(Companies.companyId==stock.id).first()   
-    data.append((company.companyName, company.industryField, list(kasvu)[1]))
+    if list(kasvu)[1]==0:
+      kid="N/A"
+    else:
+      kid=list(kasvu)[1]
+    data.append((company.companyName, company.industryField, kid))
   return render_template('kasvut.html',alku=alku, loppu=loppu, data=data)
 
 if __name__ == "__main__":
